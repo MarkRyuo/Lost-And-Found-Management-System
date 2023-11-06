804437499a6b5ac33327d3322631843a7045a579
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-06 22:11:45 +0800

    update -23

---
+++ assets/js/Landing.js
@@ -0,0 +1,8 @@
+ // Get the button element by its id
+ var homepageButton = document.getElementById("homepageButton");
+
+ // Add a click event listener to the button
+ homepageButton.addEventListener("click", function() {
+   // Redirect to the homepage URL
+   window.location.href = "index.html"; // Replace with your actual homepage URL
+ });
