7ff5a4d03c00bbd6be312aca248099651dfa4a05
Jhon Mark Malupa <143322665+MarkRyuo@users.noreply.github.com>
2023-11-06 22:35:41 +0800

    Delete assets/js/landing.js

--- assets/js/landing.js
+++
@@ -1,7 +0,0 @@
- // Landing Page
- var homepageButton = document.getElementById("homepageButton");
-
- homepageButton.addEventListener("click", function() {
-   
-   window.location.href = "index.html"; 
- });
