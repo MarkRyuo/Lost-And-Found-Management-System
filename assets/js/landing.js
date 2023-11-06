86c875dae2fa4e87a49e036f40a505edfb31007f
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-06 22:27:16 +0800

    update by jhon mark -1

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -1,8 +1,7 @@
- // Get the button element by its id
+// Landing Page Here
  var homepageButton = document.getElementById("homepageButton");
 
- // Add a click event listener to the button
  homepageButton.addEventListener("click", function() {
-   // Redirect to the homepage URL
-   window.location.href = "index.html"; // Replace with your actual homepage URL
+
+   window.location.href = "index.html";
  });
