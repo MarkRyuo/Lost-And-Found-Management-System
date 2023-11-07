af1eb6c1efedcbce44313b817d96aa2b04fb1852
Leomar-Delacruz <leomar240230@gmail.com>
2023-11-07 20:01:50 +0800

    added student login

--- studentlogin.php
+++ studentlogin.php
@@ -3,9 +3,42 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
 </head>
 <body>
-    
+
+<?php
+require_once "database.php";
+
+if (isset($_POST["login"])) {
+    if (isset($_POST["email"]) && isset($_POST["password"])) {
+        $email = mysqli_real_escape_string($conn, trim($_POST["email"]));
+        $password = mysqli_real_escape_string($conn, trim($_POST["password"]));
+
+        $sql = "SELECT * FROM tbl_student WHERE emailaddress = '$email'";
+        $result = mysqli_query($conn, $sql);
+        $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+        if ($user) {
+            if (password_verify($password, $user["password"])) {
+                header("Location: index.php");
+                die();
+            } else {
+                echo "<div class='alert alert-danger'>Password does not match</div>";
+            }
+        } else {
+            echo "<div class='alert alert-danger'>Email does not match</div>";
+        }
+    }
+}
+?>
+
+    <form action="studentlogin.php" method="post">
+        <div>Email Address:</div>
+        <input type="email"  name="email" placeholder="Email Address" >
+        <div>Password</div>
+        <input type="password" name="password" placeholder="Password"> </br>
+        <input type="submit" value="Login" name="login">
+    </form>
 </body>
 </html>
