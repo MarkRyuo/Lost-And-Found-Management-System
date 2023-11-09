fcbee0752298cc3d168b05f0a070dcbe7dd6e3fd
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-09 08:52:13 +0800

    update here

---
+++ Dashboard/Dash-Board.js
@@ -0,0 +1,19 @@
+function navigateToUserPage() {
+  window.location.href = "Link here";
+}
+
+function navigateToDashboardPage() {
+  window.location.href = "Link here";
+}
+
+function navigateToViewLostItemsPage() {
+  window.location.href = "Link here";
+}
+
+function navigateToReportMissingItemsPage() {
+  window.location.href = "Link here";
+}
+
+function navigateToClaimConfirmationPage() {
+  window.location.href = "Link here";
+}
