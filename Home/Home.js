c97ff6982c544b76072c22656e914e66a4e72e64
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-11 07:42:16 +0800

    update here

--- Home/Home.js
+++ Home/Home.js
@@ -7,7 +7,13 @@ User.addEventListener("click", function() {
   window.location.href = "/User-Profile/userProfile.html";
 });
 
-// 
+// code for viewlostItem
+
+var viewlostItem = document.getElementById("viewlostItem");
+
+viewlostItem.addEventListener("click", function() {
+  window.location.href = "/View Lost Item/ViewlostItem.html";
+});
 
 
 
