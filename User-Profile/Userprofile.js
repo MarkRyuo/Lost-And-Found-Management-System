1c2158efc445a9627172528e5ac756ca5a54e0c6
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-11 16:02:03 +0800

    update here

--- User-Profile/Userprofile.js
+++
@@ -1,7 +0,0 @@
-
-// this is the code for btn about in User
-var btnabout = document.getElementById("btnabout");
-
-btnabout.addEventListener("click", function() {
-  window.location.href = "/assets/About/About.html";
-});
