1870560d1b1d32a8c2b5a8178ad887281318f9f5
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-11 10:54:36 +0800

    update here

---
+++ assets/js/View-lost-Logout.js
@@ -0,0 +1,10 @@
+/*Dapat dito ay may condition hindi pa ako maalam
+kasi kailangan may alert*/
+
+// View-lost-Logout
+
+var ViewlostLogout = document.getElementById("ViewlostLogout");
+
+ViewlostLogout.addEventListener("click", function() {
+  window.location.href = "/Home-Page/Home-Page.html";
+});
