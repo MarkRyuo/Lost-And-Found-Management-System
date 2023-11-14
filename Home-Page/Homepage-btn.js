061840416d40aa264f2c4e8e98f3696287ec8e3e
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-14 10:06:18 +0800

    update here

---
+++ Home-Page/Homepage-btn.js
@@ -0,0 +1,24 @@
+
+// studentSignin btn 
+
+var studentSignin = document.getElementById("studentSignin");
+
+studentSignin.addEventListener("click", function() {
+  window.location.href = "#";
+});
+
+// securitySignin btn
+var securitySignin = document.getElementById("securitySignin");
+
+securitySignin.addEventListener("click", function() {
+  window.location.href = "#";
+});
+
+// adminSignin btn 
+var adminSignin = document.getElementById("adminSignin");
+
+adminSignin.addEventListener("click", function() {
+  window.location.href = "#";
+});
+
+
