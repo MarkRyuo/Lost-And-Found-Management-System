ef398233466ca58b958343b0c4ce4ee0be2fae3f
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-16 19:16:49 +0800

    add sql

---
+++ assets/Sql/db_nt3102.sql
@@ -0,0 +1,87 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Nov 16, 2023 at 11:15 AM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_nt3102`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE IF NOT EXISTS `student` (
+  `StudentId` int NOT NULL AUTO_INCREMENT,
+  `Sr_code` varchar(191) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`StudentId`),
+  UNIQUE KEY `Sr_code` (`Sr_code`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`StudentId`, `Sr_code`, `password`) VALUES
+(1, '21-36991', '21-36991'),
+(2, '21-30311', '21-30311'),
+(3, '21-23121', '21-23121'),
+(4, '21-34551', '21-34551');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbemployee`
+--
+
+DROP TABLE IF EXISTS `tbemployee`;
+CREATE TABLE IF NOT EXISTS `tbemployee` (
+  `empid` int NOT NULL,
+  `lastname` varchar(25) NOT NULL,
+  `firstname` varchar(25) NOT NULL,
+  `department` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_studentinfo`
+--
+
+DROP TABLE IF EXISTS `tb_studentinfo`;
+CREATE TABLE IF NOT EXISTS `tb_studentinfo` (
+  `studid` int NOT NULL,
+  `lastname` varchar(25) NOT NULL,
+  `firstname` varchar(25) NOT NULL,
+  `course` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tb_studentinfo`
+--
+
+INSERT INTO `tb_studentinfo` (`studid`, `lastname`, `firstname`, `course`) VALUES
+(1, 'parker', 'peter', 'bsit');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
