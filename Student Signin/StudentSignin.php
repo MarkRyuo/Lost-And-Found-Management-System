5a71d8aadd1939f8166572afa2bb85ac0c08faa7
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-16 21:55:21 +0800

    update here

--- Student Signin/StudentSignin.php
+++ Student Signin/StudentSignin.php
@@ -72,11 +72,11 @@ $conn->close();
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
 
         <label for="sr_code">Sr_code:</label>
-        <input type="text" id="sr_code" name="sr_code" pattern="\d{7}" title="Sr_code should be a 7-digit number." required><br>
+        <input type="text" id="sr_code" name="sr_code" pattern="\d{7}" title="Sr_code should be a 7-digit number and contain a hyphen(-)." required><br>
 
 
         <label for="password">Password:</label>
-        <input type="password" id="password" name="password" pattern=".*-.*" title="Password should contain a hyphen." required><br>
+        <input type="password" id="password" name="password" pattern=".*-.*" title="Password should be a 7-digit number and contain a hyphen(-)." required><br>
 
         <input type="submit" value="Submit">
     </form>
