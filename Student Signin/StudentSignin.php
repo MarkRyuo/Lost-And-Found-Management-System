f073d953654949ddcf169fe9eedd95e8174323ac
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-16 23:04:06 +0800

    update here

--- Student Signin/StudentSignin.php
+++ Student Signin/StudentSignin.php
@@ -1,69 +1,100 @@
+<?php
+// Database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "db_nt3102";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get input data
+    $sr_code = $_POST["sr_code"];
+    $password = $_POST["password"];
+
+    // Check if Sr_code follows the format 00-00000
+    if (!preg_match('/^\d{2}-\d{5}$/', $sr_code)) {
+        echo "Sr_code should follow the format 00-00000.";
+        $conn->close();
+        exit();
+    }
+
+    // Check if password contains a hyphen
+    if (strpos($password, '-') === false) {
+        echo "Password should contain a hyphen.";
+        $conn->close();
+        exit();
+    }
+
+    // Check if user exists
+    $check_user_query = "SELECT * FROM student WHERE Sr_code = '$sr_code'";
+    $result = $conn->query($check_user_query);
+
+    if ($result->num_rows > 0) {
+        // User exists, check password
+        $row = $result->fetch_assoc();
+        if ($password == $row["password"]) {
+            // Login successful, redirect to a new page
+            header("Location: /Student View Lost/StudentView.html"); // Replace with the correct path
+            exit();
+        } else {
+            echo "Incorrect password!";
+        }
+    } else {
+        // User does not exist, insert new user
+        $insert_user_query = "INSERT INTO student (Sr_code, Password) VALUES ('$sr_code', '$password')";
+        if ($conn->query($insert_user_query) === TRUE) {
+            echo "Sign up successful !";
+            // Add your login logic here
+        } else {
+            echo "Error: " . $insert_user_query . "<br>" . $conn->error;
+        }
+    }
+}
+
+$conn->close();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
+    <title>Login System</title>
 </head>
 <body>
-    <h2>Login</h2>
+    <h2>Login System</h2>
     
-    <?php
-    // PHP code for handling form submission
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Retrieve values from the form
-        $username = $_POST["username"];
-        $password = $_POST["password"];
-
-        // Validate the login (in this example, check if the username and password match)
-        if (checkIfUserExists($username, $password)) {
-            echo "<p>Login successful!</p>";
-        } else {
-            // If the user doesn't exist or the password is incorrect, display an error message
-            echo "<p>Login failed. Please check your username and password.</p>";
-        }
-    }
-
-    function checkIfUserExists($username, $password) {
-        // Implement your database check here
-        // For demonstration, assume a database connection and a users table with username and password columns
-        $servername = "your_server_name";
-        $dbname = "your_database_name";
-        $username_db = "your_database_username";
-        $password_db = "your_database_password";
-
-        try {
-            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username_db, $password_db);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-            $stmt = $conn->prepare("SELECT * FROM users WHERE username = :username AND password = :password");
-            $stmt->bindParam(':username', $username);
-            $stmt->bindParam(':password', $password); // Note: In a real application, you should hash the password before comparing
-
-            $stmt->execute();
-
-            return $stmt->rowCount() > 0;
-        } catch (PDOException $e) {
-            echo "Error: " . $e->getMessage();
-            return false;
-        } finally {
-            $conn = null;
-        }
-    }
-    ?>
+    <!-- Popup content -->
+    <div id="popup" style="display: block; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); padding: 20px; background-color: #f1f1f1; border: 1px solid #d4d4d4; border-radius: 5px; text-align: center;">
+        <p>Welcome! If it's your first time, just enter your Sr-code as a Username and password.</p>
+        <button onclick="closePopup()" id="btnpopup">OK</button>
+    </div>
 
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        <label for="username">Username:</label>
-        <input type="text" name="username" required>
 
-        <br>
+        <label for="sr_code">Sr_code:</label>
+        <input type="text" id="sr_code" name="sr_code" pattern="\d{2}-\d{5}" title="Sr_code should follow the format 00-00000." required><br>
 
         <label for="password">Password:</label>
-        <input type="password" name="password" required>
+        <input type="password" id="password" name="password" pattern="\d{2}-\d{5}" title="Password should contain a hyphen." required><br>
 
-        <br>
-
-        <input type="submit" value="Login">
+        <input type="submit" value="Signin">
+        
     </form>
+
+
 </body>
+<!-- JavaScript to hide the popup -->
+    <script>
+        function closePopup() {
+            var popup = document.getElementById('popup');
+            popup.style.display = 'none';
+        }
+    </script>
 </html>
